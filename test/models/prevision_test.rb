010273cf8d4ee2b96821e463f5bf74554dcb2093
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2015-06-06 17:36:21 -0500

    Nested attributes desde Prevision

--- test/models/prevision_test.rb
+++ test/models/prevision_test.rb
@@ -2,10 +2,14 @@ require 'test_helper'
 
 class PrevisionTest < ActiveSupport::TestCase
   should have_many(:depositos)
+  should have_many(:apartados)
+  should have_many(:topes)
   should validate_presence_of(:fecha_inicial)
   should validate_presence_of(:fecha_final)
   should validate_presence_of(:monto)
   should validate_numericality_of(:monto).is_greater_than(0)
+  should accept_nested_attributes_for(:apartados)
+  should accept_nested_attributes_for(:topes)
 
   test "should validate date ranges" do
     prevision = FactoryGirl.build(
