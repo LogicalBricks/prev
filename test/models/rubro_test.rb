523c0186178816c2168ce3917f65128296a7ce4c
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2015-06-06 16:21:07 -0500

    Agregadas pruebas faltantes a Rubro

--- test/models/rubro_test.rb
+++ test/models/rubro_test.rb
@@ -1,6 +1,7 @@
 require 'test_helper'
 
 class RubroTest < ActiveSupport::TestCase
+  should belong_to(:agrupador)
   should validate_presence_of(:nombre)
   should validate_presence_of(:descripcion)
 end
