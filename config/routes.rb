b7e8e3fba0c1d99ad55bb374b5f7fcc2bcbbe7f8
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2015-06-06 14:09:00 -0500

    Integración de bootstrap a layout y creación de home#index

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'home/index'
+
   resources :gastos
   resources :socios
   resources :proveedores
@@ -12,7 +14,7 @@ Rails.application.routes.draw do
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  root 'home#index'
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
