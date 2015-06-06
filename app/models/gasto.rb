f6e61414d303ff8930d52932e9c98a1515db15f3
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-06-06 14:03:32 -0500

    Add test to gasto.

--- app/models/gasto.rb
+++ app/models/gasto.rb
@@ -1,5 +1,10 @@
 class Gasto < ActiveRecord::Base
+  # == Associations ==
   belongs_to :socio
   belongs_to :proveedor
   belongs_to :apartado
+
+  # == Validations ==
+  validates :socio, presence: true
+  validates :monto, numericality: { greater_than: 0 }
 end
--- test/models/gasto_test.rb
+++ test/models/gasto_test.rb
@@ -1,7 +1,10 @@
 require 'test_helper'
 
 class GastoTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  should belong_to :socio
+  should belong_to :proveedor
+  should belong_to :apartado
+
+  should validate_presence_of :socio
+  should validate_numericality_of(:monto).is_greater_than(0)
 end
