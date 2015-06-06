9e0ef78d5a1cfb0af5ff37e1d3583cafbd7c4aef
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-06-06 17:28:17 -0500

    Validate apartado's monto not to rebase prevision's monto.

--- app/models/apartado.rb
+++ app/models/apartado.rb
@@ -1,9 +1,18 @@
 class Apartado < ActiveRecord::Base
+  # == Associations ==
   belongs_to :rubro
   belongs_to :prevision
 
+  # == Validations ==
   validates :rubro, :prevision, :monto_maximo, presence: true
   validates :monto_maximo, numericality: { greater_than: 0 }
+  validate :monto_no_rebasa_monto_de_prevision
+
+private
+
+  def monto_no_rebasa_monto_de_prevision
+    errors.add :monto, 'rebasa el monto total del apartado'
+  end
 end
 
 # == Schema Information
--- test/models/apartado_test.rb
+++ test/models/apartado_test.rb
@@ -5,6 +5,13 @@ class ApartadoTest < ActiveSupport::TestCase
   should validate_presence_of(:prevision)
   should validate_presence_of(:monto_maximo)
   should validate_numericality_of(:monto_maximo).is_greater_than(0)
+
+  test "should validate monto not to rebase prevision's monto" do
+    prevision = FactoryGirl.build :prevision, monto: 100
+    apartado = prevision.apartados.build monto_maximo: 101
+    refute apartado.valid?
+    assert_equal 1, apartado.errors[:monto].size
+  end
 end
 
 # == Schema Information
