b0505263d5e21f54db5a8bb123b1c53b7ea65688
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-06-06 13:25:51 -0500

    Add test for socio.

--- app/models/socio.rb
+++ app/models/socio.rb
@@ -1,3 +1,8 @@
 class Socio < ActiveRecord::Base
+  # == Associations ==
   belongs_to :usuario
+  has_many :gastos
+
+  # == Validations ==
+  validates :nombre, :usuario, presence: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,13 +11,69 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150606180140) do
+ActiveRecord::Schema.define(version: 20150606181025) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "agrupadores", force: :cascade do |t|
     t.string   "nombre"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "apartados", force: :cascade do |t|
+    t.decimal  "monto_maximo"
+    t.integer  "rubro_id"
+    t.integer  "prevision_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "apartados", ["prevision_id"], name: "index_apartados_on_prevision_id", using: :btree
+  add_index "apartados", ["rubro_id"], name: "index_apartados_on_rubro_id", using: :btree
+
+  create_table "depositos", force: :cascade do |t|
+    t.decimal  "monto"
+    t.date     "fecha"
+    t.text     "descripcion"
+    t.integer  "prevision_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "depositos", ["prevision_id"], name: "index_depositos_on_prevision_id", using: :btree
+
+  create_table "gastos", force: :cascade do |t|
+    t.string   "factura_xml"
+    t.string   "factura_pdf"
+    t.string   "solicitud"
+    t.decimal  "monto"
+    t.date     "fecha"
+    t.integer  "metodo_pago",  default: 0
+    t.text     "descripcion"
+    t.integer  "socio_id"
+    t.integer  "proveedor_id"
+    t.integer  "apartado_id"
+    t.datetime "created_at",               null: false
+    t.datetime "updated_at",               null: false
+  end
+
+  add_index "gastos", ["apartado_id"], name: "index_gastos_on_apartado_id", using: :btree
+  add_index "gastos", ["proveedor_id"], name: "index_gastos_on_proveedor_id", using: :btree
+  add_index "gastos", ["socio_id"], name: "index_gastos_on_socio_id", using: :btree
+
+  create_table "previsiones", force: :cascade do |t|
+    t.date     "fecha_inicial"
+    t.date     "fecha_final"
+    t.decimal  "monto"
+    t.datetime "created_at",    null: false
+    t.datetime "updated_at",    null: false
+  end
+
+  create_table "proveedores", force: :cascade do |t|
+    t.string   "nombre"
+    t.string   "rfc"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
@@ -32,6 +88,25 @@ ActiveRecord::Schema.define(version: 20150606180140) do
     t.integer "rol_id",     null: false
     t.integer "usuario_id", null: false
   end
+
+  create_table "rubros", force: :cascade do |t|
+    t.string   "nombre"
+    t.text     "descripcion"
+    t.integer  "agrupador_id"
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+  end
+
+  add_index "rubros", ["agrupador_id"], name: "index_rubros_on_agrupador_id", using: :btree
+
+  create_table "socios", force: :cascade do |t|
+    t.string   "nombre"
+    t.integer  "usuario_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "socios", ["usuario_id"], name: "index_socios_on_usuario_id", using: :btree
 
   create_table "usuarios", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -51,4 +126,12 @@ ActiveRecord::Schema.define(version: 20150606180140) do
   add_index "usuarios", ["email"], name: "index_usuarios_on_email", unique: true, using: :btree
   add_index "usuarios", ["reset_password_token"], name: "index_usuarios_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "apartados", "previsiones"
+  add_foreign_key "apartados", "rubros"
+  add_foreign_key "depositos", "previsiones"
+  add_foreign_key "gastos", "apartados"
+  add_foreign_key "gastos", "proveedores"
+  add_foreign_key "gastos", "socios"
+  add_foreign_key "rubros", "agrupadores"
+  add_foreign_key "socios", "usuarios"
 end
