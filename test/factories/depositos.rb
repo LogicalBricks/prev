a8aa0627fc1094fd958fd88395c43de740705f26
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-06-06 17:32:00 -0500

    Regenerate annotations.

--- test/factories/depositos.rb
+++ test/factories/depositos.rb
@@ -20,11 +20,3 @@ end
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-# Indexes
-#
-#  index_depositos_on_prevision_id  (prevision_id)
-#
-# Foreign Keys
-#
-#  fk_rails_3728a922f6  (prevision_id => previsiones.id)
-#
--- test/models/apartado_test.rb
+++ test/models/apartado_test.rb
@@ -25,13 +25,3 @@ end
 #  created_at   :datetime         not null
 #  updated_at   :datetime         not null
 #
-# Indexes
-#
-#  index_apartados_on_prevision_id  (prevision_id)
-#  index_apartados_on_rubro_id      (rubro_id)
-#
-# Foreign Keys
-#
-#  fk_rails_a7b482130a  (rubro_id => rubros.id)
-#  fk_rails_b96ecf8df7  (prevision_id => previsiones.id)
-#
