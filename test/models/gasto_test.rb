5021024c8ce68c6c66b3eab35508af7c6c5ea6cd
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-06-06 15:53:38 -0500

    Add test to gasto.

--- test/models/gasto_test.rb
+++ test/models/gasto_test.rb
@@ -6,7 +6,29 @@ class GastoTest < ActiveSupport::TestCase
   should belong_to :apartado
 
   should validate_presence_of :socio
+  should validate_presence_of :apartado
   should validate_numericality_of(:monto).is_greater_than(0)
+
+  def stub_fecha_inicial_and_fecha_final(apartado, fecha_inicial, fecha_final)
+    def apartado.fecha_inicial; Date.today; end
+    def apartado.fecha_final; Date.today.tomorrow; end
+  end
+
+  test "does not allow to set a fecha before the prevision's innitial date" do
+    apartado = FactoryGirl.build_stubbed :apartado
+    stub_fecha_inicial_and_fecha_final(apartado, Date.today, Date.today.tomorrow)
+    gasto = FactoryGirl.build :gasto, fecha: 1.month.ago, apartado: apartado
+    gasto.valid?
+    assert_equal 1, gasto.errors[:fecha].size
+  end
+
+  test "does not allow to set a fecha after the prevision's final date" do
+    apartado = FactoryGirl.build_stubbed :apartado
+    stub_fecha_inicial_and_fecha_final(apartado, Date.today, Date.today.tomorrow)
+    gasto = FactoryGirl.build :gasto, fecha: 1.month.from_now, apartado: apartado
+    gasto.valid?
+    assert_equal 1, gasto.errors[:fecha].size
+  end
 end
 
 # == Schema Information
