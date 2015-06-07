5d7eca3e5622cde6d1f140d120897fbd65270816
Fernando Villalobos <fernando.villalobos@logicalbricks.com>
2015-06-07 18:27:06 -0500

    Ajustes a vistas de Registro

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,13 @@
 module ApplicationHelper
+  def mark_for value
+    value ? check_mark : cross_mark
+  end
+
+  def check_mark
+    content_tag :i, '', class: 'fa fa-check text-success'
+  end
+
+  def cross_mark
+    content_tag :i, '', class: 'fa fa-times text-danger'
+  end
 end
--- app/helpers/gastos_helper.rb
+++ app/helpers/gastos_helper.rb
@@ -1,2 +1,11 @@
 module GastosHelper
+  def link_to_file objeto, metodo
+    if objeto.send "#{ metodo }?"
+      link_to objeto.send(metodo).url, class: 'btn btn-xs btn-success', target: :blank do
+        content_tag(:i, '', class: 'fa fa-download') + ' Descargar archivo'
+      end
+    else
+      cross_mark
+    end
+  end
 end
