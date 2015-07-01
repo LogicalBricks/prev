a9c1ccb6fb5f57891233e9448e608609255fb5db
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-01 10:29:42 -0500

    Add specific home views to show when some resource is missing.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,9 @@
 class HomeController < ApplicationController
   def index
+    render :no_rubro and return if Rubro.count == 0
+    render :no_socio and return if Socio.count == 0
+    render :no_prevision and return if Prevision.count == 0
+
     @prevision = Prevision.first
-    render :blank unless @prevision
   end
 end
