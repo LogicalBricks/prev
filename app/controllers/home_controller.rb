9145edff91229a0118ef239c72dbe39d054067d6
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-07 10:13:39 -0500

    Add no_deposito and fix home#index view.

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,8 +1,9 @@
 class HomeController < ApplicationController
   def index
-    render :no_rubro and return if Rubro.count == 0
     render :no_socio and return if Socio.count == 0
+    render :no_rubro and return if Rubro.count == 0
     render :no_prevision and return if Prevision.count == 0
+    render :no_deposito and return if Deposito.count == 0
 
     @prevision = Prevision.first
   end
