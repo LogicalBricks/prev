fd150067145e80869136eb8620f4c7235fa38230
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-14 09:47:25 -0500

    Improve date_range's API.

--- app/models/date_range.rb
+++ app/models/date_range.rb
@@ -2,14 +2,26 @@ class DateRange
   attr_reader :initial, :final, :year
 
   def initialize(initial: nil, final: nil, year: nil)
-    @year = year
-    @initial = initial
-    @final = final
-    calculate_initial_and_final_dates if @year
+    if year
+      @year = year
+      calculate_initial_and_final_dates
+    else
+      raise unless initial and final
+      @initial = initial
+      @final = final
+    end
   end
 
   def to_range
     @initial..@final
+  end
+
+  def consistent?
+    @initial < @final
+  end
+
+  def includes?(date)
+    to_range.cover? date
   end
 
 private
--- test/models/date_range_test.rb
+++ test/models/date_range_test.rb
@@ -2,13 +2,13 @@ require 'test_helper'
 
 class DateRangeTest < Minitest::Test
   def test_stores_the_initial_date
-    date_range = DateRange.new initial: Date.today
+    date_range = DateRange.new initial: Date.today, final: Date.today.tomorrow
     assert_equal Date.today, date_range.initial
   end
 
   def test_stores_the_final_date
-    date_range = DateRange.new final: Date.today
-    assert_equal Date.today, date_range.final
+    date_range = DateRange.new initial: Date.today, final: Date.today.tomorrow
+    assert_equal Date.today.tomorrow, date_range.final
   end
 
   def test_stores_the_year
@@ -26,11 +26,45 @@ class DateRangeTest < Minitest::Test
     assert_equal '2014-12-31'.to_date, date_range.final
   end
 
+  def test_an_excetion_is_raised_if_initial_or_final_is_not_set
+    assert_raises do
+      DateRange.new
+    end
+
+    assert_raises do
+      DateRange.new initial: Date.today
+    end
+
+    assert_raises do
+      DateRange.new final: Date.today
+    end
+  end
+
   def test_to_range_is_the_range_between_dates
     date_range = DateRange.new year: 2014
     assert_equal '2014-01-01'.to_date..'2014-12-31'.to_date, date_range.to_range
 
     date_range = DateRange.new year: 2015
     assert_equal '2015-01-01'.to_date..'2015-12-31'.to_date, date_range.to_range
+  end
+
+  def test_consistent_is_true_if_initial_is_before_final
+    date_range = DateRange.new initial: Date.today, final: Date.today.tomorrow
+    assert date_range.consistent?
+  end
+
+  def test_consistent_is_false_if_initial_is_after_final
+    date_range = DateRange.new initial: Date.today, final: Date.today.yesterday
+    refute date_range.consistent?
+  end
+
+  def test_includes_is_true_if_the_given_date_is_in_the_date_range
+    date_range = DateRange.new initial: Date.today.yesterday, final: Date.today.tomorrow
+    assert date_range.includes? Date.today
+  end
+
+  def test_includes_is_false_if_the_given_date_is_not_in_the_date_range
+    date_range = DateRange.new initial: Date.today.yesterday, final: Date.today.tomorrow
+    refute date_range.includes? 1.month.ago
   end
 end
