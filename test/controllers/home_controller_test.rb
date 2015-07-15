dbbdd764034f993212a8ffff5d5d32585703c127
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-15 09:30:34 -0500

    Extract templates to _no_resource in order to reuse its functionality.

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,8 +1,35 @@
 require 'test_helper'
 
 class HomeControllerTest < ActionController::TestCase
-  test "should get index" do
+  test 'should render no_socio when no socio is in the system' do
     get :index
+    assert_template 'no_socio' 
+  end
+
+  test 'should render no_rubro when no rubro is in the system' do
+    FactoryGirl.create :socio
+    get :index
+    assert_template 'no_rubro' 
+  end
+
+  test 'should render no_prevision when no prevision is in the system' do
+    FactoryGirl.create :socio
+    FactoryGirl.create :rubro
+    get :index
+    assert_template 'no_prevision' 
+  end
+
+  test 'should render no_deposito when no deposito is in the system' do
+    FactoryGirl.create :socio
+    FactoryGirl.create :rubro
+    FactoryGirl.create :prevision
+    get :index
+    assert_template 'no_deposito' 
+  end
+
+  test "should get estado_cuenta" do
+    FactoryGirl.create :prevision
+    get :estado_cuenta
     assert_response :success
   end
 
