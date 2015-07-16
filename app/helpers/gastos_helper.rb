ea0f7cf34a6aa11caeb7c1a129b50b8d2d7ad18f
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-16 13:46:40 -0500

    Allow downloading files from gastos#index.
    Refactor gastos_helper#link_to_file to support a new markup.

--- app/helpers/gastos_helper.rb
+++ app/helpers/gastos_helper.rb
@@ -1,11 +1,25 @@
 module GastosHelper
-  def link_to_file objeto, metodo
+  def link_to_file objeto, metodo, format: :default
     if objeto.send "#{ metodo }?"
-      link_to objeto.send(metodo).url, class: 'btn btn-xs btn-success', target: :blank do
-        content_tag(:i, '', class: 'fa fa-download') + ' Descargar archivo'
+      if format == :default
+        default_link_to_file(objeto, metodo)
+      elsif format == :short
+        short_link_to_file(objeto, metodo)
       end
     else
       cross_mark
+    end
+  end
+
+  def default_link_to_file(objeto, metodo)
+    link_to objeto.send(metodo).url, class: 'btn btn-xs btn-success', target: :blank do
+      content_tag(:i, '', class: 'fa fa-download') + ' Descargar archivo'
+    end
+  end
+
+  def short_link_to_file(objeto, metodo)
+    link_to objeto.send(metodo).url, target: :blank do
+      check_mark
     end
   end
 end
