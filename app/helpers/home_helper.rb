32c814af10a65a4bfb1d3d7ce65d8f4074dd9e04
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-07-26 16:31:45 -0500

    Create EstadoCuenta class as a iterator helper for movimientos.

--- app/helpers/home_helper.rb
+++ app/helpers/home_helper.rb
@@ -1,2 +1,22 @@
 module HomeHelper
+  def estado_cuenta(movimientos=@movimientos)
+    @estado_cuenta ||= EstadoCuenta.new(movimientos)
+  end
+
+  class EstadoCuenta
+    attr_reader :total_cargos, :total_abonos
+
+    def initialize(movimientos)
+      @movimientos = movimientos
+      @total_cargos = @total_abonos = 0.0
+    end
+
+    def each
+      @movimientos.each do |v|
+        @total_cargos += v.cargo.to_f
+        @total_abonos += v.abono.to_f
+        yield v
+      end
+    end
+  end
 end
