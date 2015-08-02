1c63b4a0098a7b172e6747efef793f14a80c3774
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-08-02 13:19:02 -0500

    Move EstadoCuenta class to lib.

---
+++ lib/support/estado_cuenta.rb
@@ -0,0 +1,16 @@
+class EstadoCuenta
+  attr_reader :total_cargos, :total_abonos
+
+  def initialize(movimientos)
+    @movimientos = movimientos
+    @total_cargos = @total_abonos = 0.0
+  end
+
+  def each
+    @movimientos.each do |v|
+      @total_cargos += v.cargo.to_f
+      @total_abonos += v.abono.to_f
+      yield v
+    end
+  end
+end
