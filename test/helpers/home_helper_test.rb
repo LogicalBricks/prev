afc5e35a902c881cbf6e22375aadd12b0aee2e58
Azarel Doroteo Pacheco <azareldp@gmail.com>
2015-08-04 12:52:38 -0500

    Fix tests to fit the new algorithm.

--- test/helpers/home_helper_test.rb
+++ test/helpers/home_helper_test.rb
@@ -9,24 +9,24 @@ class HomeHelperTest < ActionView::TestCase
     assert_equal html, mostrar_montos(monto: 11, monto_limite: 321)
   end
 
-  test 'should add riesgo-algo class to monto if it is close to monto_limite' do
+  test 'should add riesgo-alto class to monto if it is close to monto_limite' do
     html = '<span class="riesgo-alto">$120.00</span> / $123.00'
     assert_equal html, mostrar_montos(monto: 120.0, monto_limite: 123)
 
-    html = '<span class="riesgo-alto">$80.00</span> / $100.00'
-    assert_equal html, mostrar_montos(monto: 80, monto_limite: 100)
+    html = '<span class="riesgo-alto">$80.01</span> / $100.00'
+    assert_equal html, mostrar_montos(monto: 80.01, monto_limite: 100)
   end
 
   test 'should add riesgo-moderado class to monto if it is about 50% of monto_limite' do
-    html = '<span class="riesgo-moderado">$40.00</span> / $100.00'
-    assert_equal html, mostrar_montos(monto: 40.0, monto_limite: 100)
+    html = '<span class="riesgo-moderado">$40.01</span> / $100.00'
+    assert_equal html, mostrar_montos(monto: 40.01, monto_limite: 100)
 
-    html = '<span class="riesgo-moderado">$79.99</span> / $100.00'
-    assert_equal html, mostrar_montos(monto: 79.99, monto_limite: 100)
+    html = '<span class="riesgo-moderado">$80.00</span> / $100.00'
+    assert_equal html, mostrar_montos(monto: 80.00, monto_limite: 100)
   end
 
   test 'should ommit monto_limite if the ocultar_limite flag is true' do
-    html = '<span class="riesgo-moderado">$40.00</span>'
-    assert_equal html, mostrar_montos(monto: 40.0, monto_limite: 100, ocultar_limite: true)
+    html = '<span class="riesgo-moderado">$40.01</span>'
+    assert_equal html, mostrar_montos(monto: 40.01, monto_limite: 100, ocultar_limite: true)
   end
 end
