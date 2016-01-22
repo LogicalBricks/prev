f827c69b8fa321d88f3c1ba0811ff8655048f4eb
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2016-01-22 12:12:09 -0600

    Include javascript for bootstrap.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,6 +13,7 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require bootstrap-sprockets
 //= require showdown
 //= require jquery_nested_form
 //= require bootstrap-datepicker
