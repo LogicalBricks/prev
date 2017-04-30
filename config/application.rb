d84693230848a97d5115dcc4c85652a2132fa310
Azarel Doroteo Pacheco <azarel.doroteo@logicalbricks.com>
2017-04-30 13:14:34 -0500

    Move lib classes to app.
    Apparently, this is the new rails 5 way to do it :/

--- config/application.rb
+++ config/application.rb
@@ -11,7 +11,6 @@ module Prev
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
-    config.i18n.default_locale = 'es-MX'
-    config.autoload_paths += %W(#{config.root}/lib/support)
+    config.i18n.default_locale = "es-MX"
   end
 end
